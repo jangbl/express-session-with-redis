c362247854ce1689dd518964c46d1966f384e7db
productioncoder <productioncoding@gmail.com>
2021-06-05 13:54:19 +0200

    set same site attribute for cookies to lax

--- middleware/session.js
+++ middleware/session.js
@@ -5,14 +5,18 @@ const redisClient = require('../db/redis');
 const RedisStore = connectRedis(session);
 
 module.exports = session({
-    store: new RedisStore({client: redisClient}),
-    secret: 'mySecret',
-    saveUninitialized: false,
-    resave: false, 
-    name: 'sessionId',
-    cookie: {
-        secure: false, // if true: only transmit cookie over https, in prod, always activate this
-        httpOnly: true, // if true: prevents client side JS from reading the cookie
-        maxAge: 1000 * 60 * 30 // session max age in milliseconds
-    }
+  store: new RedisStore({ client: redisClient }),
+  secret: 'mySecret',
+  saveUninitialized: false,
+  resave: false,
+  name: 'sessionId',
+  cookie: {
+    secure: false, // if true: only transmit cookie over https, in prod, always activate this
+    httpOnly: true, // if true: prevents client side JS from reading the cookie
+    maxAge: 1000 * 60 * 30, // session max age in milliseconds
+    // explicitly set cookie to lax
+    // to make sure that all cookies accept it
+    // you should never use none anyway
+    sameSite: 'lax',
+  },
 });
